8fd968d6edbc34c2e6548fec84ce24b7ce69648a
Kevin Esteban Sánchez Torres <kevin.sanchezt07@gmail.com>
2025-08-28 09:53:22 -0500

    se inserta el archivo con las conexiones (primeros 3 ejercicios)

---
+++ conexiones.pl
@@ -0,0 +1,18 @@
+esta_conectado(vancouver, edmonton,16).
+esta_conectado(edmonto, saskaton,12).
+esta_conectado(saskatoon, winnipeg,20).
+esta_conectado(saskatoon, calgary,9).
+esta_conectado(regina, winnipeg,4).
+esta_conectado(calgary, edmonton,4).
+esta_conectado(vancouver, calgary,13).
+esta_conectado(calgary, regina,14).
+esta_conectado(regina, saskatoon,7).
+
+verificar_conexion(X,Y) :- 
+    esta_conectado(X,Y,_).
+
+esta_conectado_con(X,Y,Z) :-
+    esta_conectado(X,Y,Z).
+
+tiene_aristas(X) :-
+    esta_conectado(X,_,_).
